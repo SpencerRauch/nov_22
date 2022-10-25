e5618bcbd600c436afb45ccfe1322d707ff3afd0
Spencer Rauch <rauchs@gmail.com>
2022-10-25 06:23:59 -0700

    mornin algo

---
+++ algos/W1D2.js
@@ -0,0 +1,35 @@
+/* 
+  String: Reverse
+  Given a string,
+  return a new string that is the given string reversed
+*/
+
+const str1 = "creature";
+const expected1 = "erutaerc";
+
+const str2 = "dog";
+const expected2 = "god";
+
+const str3 = "hello";
+const expected3 = "olleh";
+
+const str4 = "";
+const expected4 = "";
+
+/**
+ * Reverses the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str String to be reversed.
+ * @returns {string} The given str reversed.
+ */
+function reverseString(str) {
+    //Your code here
+}
+
+//TEST CODE FOR REVERSE
+console.log(reverseString(str1)) // Expected: erutaerc
+console.log(reverseString(str2)) // Expected: god
+console.log(reverseString(str3)) // Expected: olleh
+console.log(reverseString(str4)) // Expected: ""
+
