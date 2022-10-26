5da17293069af923aa203d703b919aae66a24bc1
Spencer Rauch <rauchs@gmail.com>
2022-10-26 06:56:24 -0700

    mornin algo

---
+++ algos/W1D3.js
@@ -0,0 +1,82 @@
+/* 
+  Given an arr and a separator string, output a string of every item in the array separated by the separator.
+  
+  No trailing separator at the end
+  Default the separator to a comma with a space after it if no separator is provided
+*/
+
+const arr1 = [1, 2, 3];
+const separator1 = ", ";
+const expected1 = "1, 2, 3";
+
+const arr2 = [1, 2, 3];
+const separator2 = "-";
+const expected2 = "1-2-3";
+
+const arr3 = [1, 2, 3];
+const separator3 = " - ";
+const expected3 = "1 - 2 - 3";
+
+const arr4 = [1];
+const separator4 = ", ";
+const expected4 = "1";
+
+const arr5 = [];
+const separator5 = ", ";
+const expected5 = "";
+
+/**
+ * Converts the given array into a string of items separated by the given separator.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<string|number|boolean>} arr The items to be joined as a string.
+ * @param {string} separator To separate each item of the given arr.
+ * @returns {string} The given array items as a string separated by the given separator.
+ */
+function join(arr, separator) {
+    //Your code here
+}
+
+console.log(join(arr1, separator1)) // Expected: "1, 2, 3"
+console.log(join(arr2, separator2)) // Expected: "1-2-3"
+console.log(join(arr3, separator3)) // Expected: "1 - 2 - 3"
+console.log(join(arr4, separator4)) // Expected: "1"
+
+/* 
+  Acronyms
+  Create a function that, given a string, returns the string’s acronym 
+  (first letter of each word capitalized). 
+  Do it with .split first if you need to, then try to do it without
+*/
+
+const str1 = "object oriented programming";
+const expectedA = "OOP";
+
+// The 4 pillars of OOP
+const str2 = "abstraction polymorphism inheritance encapsulation";
+const expectedB = "APIE";
+
+const str3 = "software development life cycle";
+const expectedC = "SDLC";
+
+// Bonus: ignore extra spaces
+const str4 = "  global   information tracker    ";
+const expectedD = "GIT";
+
+/**
+ * Turns the given str into an acronym.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} wordsStr A string to be turned into an acronym.
+ * @returns {string} The acronym.
+ */
+ function acronymize(str) {
+    //Your code here
+}
+
+
+
+console.log(acronymize(str1)); // Expected: "OOP"
+console.log(acronymize(str2)); // Expected: "APIE"
+console.log(acronymize(str3)); // Expected: "SDLC"
+console.log(acronymize(str4)); // Expected: "GIT"
