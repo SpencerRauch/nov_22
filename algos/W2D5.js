b279e8521d4b6223e598aad6e2d3a230ca0127bd
Spencer Rauch <rauchs@gmail.com>
2022-11-04 08:49:55 -0700

    solutions

--- algos/W2D5.js
+++ algos/W2D5.js
@@ -8,18 +8,18 @@
 */
 
 const newInv1 = [
-    { name: "Grain of Rice", quantity: 9000 },
-    { name: "Peanut Butter", quantity: 50 },
-    { name: "Royal Jelly", quantity: 20 },
+  { name: "Grain of Rice", quantity: 9000 },
+  { name: "Peanut Butter", quantity: 50 },
+  { name: "Royal Jelly", quantity: 20 },
 ];
 const currInv1 = [
-    { name: "Peanut Butter", quantity: 20 },
-    { name: "Grain of Rice", quantity: 1 },
+  { name: "Peanut Butter", quantity: 20 },
+  { name: "Grain of Rice", quantity: 1 },
 ];
 const expected1 = [
-    { name: "Peanut Butter", quantity: 70 },
-    { name: "Grain of Rice", quantity: 9001 },
-    { name: "Royal Jelly", quantity: 20 },
+  { name: "Peanut Butter", quantity: 70 },
+  { name: "Grain of Rice", quantity: 9001 },
+  { name: "Royal Jelly", quantity: 20 },
 ];
 
 
@@ -49,12 +49,82 @@ const expected3 = [{ name: "Peanut Butter", quantity: 20 }];
  * @param {Array<Inventory>} currInv
  * @return The currInv after being updated.
  */
- function updateInventory(newInv, currInv) {
-  //Your code here
+function updateInventory(newInv, currInv) {
+  if (newInv == []) {
+    return currInv
+  }
+  if (currInv == []) {
+    return newInv
+  }
+  for (let x = 0; x < newInv.length; x++) {
+    let match = false
+    for (let i = 0; i < currInv.length; i++) {
+
+      if (newInv[x]['name'] == currInv[i]['name']) {
+        currInv[i]['quantity'] += newInv[x]['quantity']
+        match = true
+        break
+      }
+    } if (!match) {
+      currInv.push(newInv[x])
+    }
+  } return currInv
 }
+
 
 
 console.log(updateInventory(newInv1, currInv1))
 console.log(updateInventory(newInv2, currInv2))
 console.log(updateInventory(newInv3, currInv3))
 
+
+
+
+
+
+function updateInventory(newInv, currInv) {
+  for (let newEntry of newInv) { //iterate over items to be added into inventory
+    let match = false; //set a flag to keep track of if we find a matching item in our currInv
+    for (let currEntry of currInv) { //inner loop: iterating over existing items in inventory
+      if (currEntry['name'] == newEntry['name']) { //if we find a matching entry
+        match = true; //set match to true
+        currEntry['quantity'] += newEntry['quantity']; //update quantities in our inventory
+        break; //stop the loop since we've found our match
+      }
+    }
+    if (!match) { //item didn't exist in our currInv
+      currInv.push(newEntry) //so we add it
+    }
+  }
+  return currInv;
+}
+
+
+
+
+
+
+function updateInventory2(newInv, currentInv) {
+  const currInvTable = {};
+
+  for (let i = 0; i < currentInv.length; i++) {
+    // save a reference to this currentInv item into our hash table object
+    // so we can look it up with O(1) constant time
+    currInvTable[currentInv[i].name] = currentInv[i];
+  }
+
+  for (let i = 0; i < newInv.length; i++) {
+    const item = newInv[i];
+
+    if (currInvTable.hasOwnProperty(item.name)) {
+      // retrieve the currentInv obj reference
+      let currentInvItem = currInvTable[item.name];
+
+      // update the obj by reference
+      currentInvItem.quantity += item.quantity;
+    } else {
+      currentInv.push(item);
+    }
+  }
+  return currentInv;
+}
