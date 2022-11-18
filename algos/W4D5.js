a44b0f58bd0c5430343cae4d755f9204ff44b6e9
Spencer Rauch <rauchs@gmail.com>
2022-11-18 06:52:28 -0800

    final algo!

---
+++ algos/W4D5.js
@@ -0,0 +1,28 @@
+/* 
+  String Anagrams
+  Given a string,
+  return array where each element is a string representing a different anagram (a different sequence of the letters in that string).
+  Ok to use built in methods
+*/
+
+const str1 = "lim";
+const expected1 = ["ilm", "iml", "lim", "lmi", "mil", "mli"];
+// Order of the output array does not matter
+
+/**
+ * Add params if needed for recursion.
+ * Generates all anagrams of the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {Array<string>} All anagrams of the given str.
+ */
+// You will need more parameters!
+function generateAnagrams(str) {
+  //Your code here
+//base case?
+// recursive calls?
+}
+
+console.log(generateAnagrams(str1)) //["ilm", "iml", "lim", "lmi", "mil", "mli"] (order may vary, that's okay)
+
